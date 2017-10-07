59b02c740680444dc57dd99bd8352aa3fc084d88
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-06 16:39:16 -1000

    POST returns JSON

--- app.js
+++ app.js
@@ -2,13 +2,32 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const router = express.Router();
 
 app.use('/', router);
 
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.route('/buzzword')
+  .get((req, res) => {
+    res.send("TEST");
+  })
+  .post((req, res) => {
+    console.log(req.body); // the JSON
+    res.send(req.body); //echo the result
+   // app.use(bodyParser.urlencoded({ "success": true }));
+  })
+  .put((req, res) => {
+    res.send("put done");
+  })
+  .delete((req, res) => {
+    res.send("delete done");
+  });
 
 app.get('/', (req, res) => {
-  res.send("hello, world,");
+  res.send("index.html");
 });
 
 const server = app.listen(3000, () => {
